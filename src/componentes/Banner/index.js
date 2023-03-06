9041d7834406c1cfb72e1a5e13f7dd44fd725e97
Anderson Viana <andersonviana50@gmail.com>
2023-03-06 17:05:23 -0300

    feat: formulário de criação de cards

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Banner.css'
 
-function Banner() {
+const Banner  = () => {
   return (
     <header className='banner'>
       <img src='/imagens/banner.png' alt='O banner principal da página do Organo' />
