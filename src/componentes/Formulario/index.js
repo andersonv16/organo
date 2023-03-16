5b7281301ba3f7bd32d993e9b6c1088e90ff6d1f
Anderson Viana <andersonviana50@gmail.com>
2023-03-16 01:37:06 -0300

    feat: criação do hook useState

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botão from "../Botão";
 
 
 const Formulario = () => {
+
+  const [nome, setNome] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [time, setTime] = useState('')
+
+  const itens = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'Ux e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const aoSalvar = (evento) =>{
+    evento.preventDefault()
+    console.log('Form foi submetido =>', nome, cargo, imagem, time)
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}> 
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-      </form>
+        <CampoTexto valor={nome} aoAlterado={valor => setNome(valor)} obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto valor={cargo} aoAlterado={valor => setCargo(valor)}obrigatorio={true}label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto valor={imagem} aoAlterado={valor => setImagem(valor)}label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa  valor={time} aoAlterado={valor => setTime(valor)}obrigatorio={true}label="Time" itens={itens}/>
+        <Botão> Criar card</Botão>
+        </form>
     </section>
   );
 };
